3a4154cffa4ea60f5b70a1799384706826b30e29
webgame2025 <desert.of.enjoy@gmail.com>
2025-07-20 21:14:50 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,19 @@
-const CACHE_NAME = 'code-editor-cache-v1';
-const urlsToCache = [
-  'index.html',
-  'manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/919/919827.png'
-];
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open('code-editor-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './preview.html',
+        './manifest.json',
+        './sw.js'
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
